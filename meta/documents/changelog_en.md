d0b18a8780b899f2eed5cb91b345f2a83ec6c894
Spegeli <andreas.spegel@me.com>
2018-04-26 15:10:04 +0200

    Initial Release
    Initial Release

---
+++ meta/documents/changelog_en.md
@@ -0,0 +1,6 @@
+# Release Notes for PluginExportFormatTutorial
+
+## v1.0.0 (2018-01-12)
+
+### Added
+- initial version
---
+++ src/ExportFormatServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace PluginExportFormatMotorradbekleidungNET;
+
+use Plenty\Modules\DataExchange\Services\ExportPresetContainer;
+use Plenty\Plugin\ServiceProvider;
+
+/**
+ * Class ExportFormatServiceProvider
+ * @package PluginExportFormatMotorradbekleidungNET
+ */
+class ExportFormatServiceProvider extends ServiceProvider
+{
+    /**
+     * Abstract function for registering the service provider.
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Adds the export format to the export container.
+     *
+     * @param ExportPresetContainer $container
+     */
+    public function boot(ExportPresetContainer $container)
+    {
+        $container->add(
+            'ExportFormat',
+            'PluginExportFormatMotorradbekleidungNET\ResultField\ExportFormatResultFields',
+            'PluginExportFormatMotorradbekleidungNET\Generator\ExportFormatGenerator',
+            '',
+            true,
+			true,
+            'item'
+        );
+    }
+}
