da1bafb4d35946a0665169da21cd5e4cf5152a17
Spegeli <andreas.spegel@me.com>
2018-04-30 01:59:39 +0200

    Test

--- src/Helper/PropertyHelper.php
+++ src/Helper/PropertyHelper.php
@@ -142,8 +142,10 @@ class PropertyHelper
                     {
                         if(is_array($property['texts']))
                         {
-                            //$list[] = $property['texts'][0]['value'];
+							$list[] = $property['texts'];
+                            $list[] = $property['texts'][0]['value'];
 							$list[] = $property['valueTexts'];
+							$list[] = $property['valueTexts'][0]['value'];
                         }
                     }
 
