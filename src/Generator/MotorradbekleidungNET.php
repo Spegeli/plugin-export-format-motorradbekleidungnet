c6ee6eb9bc36df60dfd754dcc65daf02e07d0a9e
Spegeli <andreas.spegel@me.com>
2018-04-30 18:05:37 +0200

    DrivingStyle Active Typo Fix

--- src/Generator/MotorradbekleidungNET.php
+++ src/Generator/MotorradbekleidungNET.php
@@ -181,9 +181,8 @@ class MotorradbekleidungNET extends CSVPluginGenerator
                         }
 
                         $attributesvaluecombi = $this->getAttributeValueCombination($variation, $settings);
-						
-						
-						$drivingstylevalue = $this->configRepository->get('ElasticExportMotorradbekleidungNET.settings.driverstyle_active') ? $this->getDrivingStyleValue($variation, $settings) : '';
+												
+						$drivingstylevalue = $this->configRepository->get('ElasticExportMotorradbekleidungNET.settings.drivingstyle_active') ? $this->getDrivingStyleValue($variation, $settings) : '';
 						$gendervalue = $this->getGenderValue($variation, $settings);
                         $colorvalue = $this->getColorValue($variation, $settings);
                         $sizevalue = $this->getSizeValue($variation, $settings);
